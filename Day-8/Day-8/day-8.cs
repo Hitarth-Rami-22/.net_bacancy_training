4c1a8f9de75b0b8219bcab75c05f517d88635d3b
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-02-08 22:58:43 +0530

    oops day-8 task

---
+++ Day-8/Day-8/day-8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_8
+{
+    internal class day_8
+    {
+        static void Main()
+        {
+            IPrinter printer = new BasePrinter();
+            printer.Print();
+
+            //Main top-level classes cannot be nested inside another class 
+        }
+    }
+
+    interface IPrinter
+        {
+            void Print();
+            void Scan();
+            void Fax();
+
+        }
+        class BasePrinter : IPrinter
+        {
+            public void Print()
+            {
+                Console.WriteLine("Printing...");
+            }
+            public void Scan()
+            {
+                throw new NotSupportedException();
+            }
+            public void Fax()
+            {
+                throw new NotSupportedException();
+            }
+
+        }
+}
