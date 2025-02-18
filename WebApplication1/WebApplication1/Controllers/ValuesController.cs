02ac350544f6704625fb43d30644ce0e7c540861
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-02-18 18:29:54 +0530

    dot net core task-1

---
+++ WebApplication1/WebApplication1/Controllers/ValuesController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ValuesController : ControllerBase
+{
+    [HttpPost("save")]
+    public bool SaveData([FromBody] object data)
+    {
+        try
+        {
+            Console.WriteLine(data.ToString());
+            string filePath = "SaveOutput.txt";
+            
+                using (StreamWriter streamwriter = new StreamWriter(filePath, true))
+                {
+                    streamwriter.WriteLine(data.ToString());
+                }
+            
+            return true;
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ValuesController1.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Weather.NET;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController1 : ControllerBase
+    {
+
+        [HttpPost("location/{city}")]
+        public IActionResult WeatherPost(string city)
+        {
+            try 
+            { string API_Key = "3a7fd9b0b9f9b0b95d1f307e2e8d3d67";
+                WeatherClient wether = new WeatherClient(API_Key);
+                var currentweather = wether.GetCurrentWeather(city);
+                return Ok(currentweather);
+
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
