ff04b14012ec56e1f4a083283a4a8ca6631a0b86
Hitarth-Rami-22 <hitarth.rami@bacancy.com>
2025-03-07 16:18:47 +0530

    Task 1 EF Core

---
+++ EF_Task/EF_Task/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace EF_Task.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server=HITSS22\\SQLEXPRESS;Database=EFAssignmentDB;Trusted_Connection=True;");
+        //}
+    }
+}
